995fc0424543dd7e76302e6b0040a32df4deba39
musanri <1335287739@qq.com>
2021-06-23 19:32:11 +0800

    后端Django

--- backEnd/library/message/models.py
+++ backEnd/library/message/models.py
@@ -5,6 +5,7 @@ from django.db import models
 #常量
 MAX_LENTH_ID = 64
 MAX_LENTH_PASSWORD = 64
+MAX_LENTH_STRING = 64
 
 # 建立数据库中的表格
 
@@ -13,3 +14,7 @@ class User(models.Model):
     objects = models.Manager()
     userID = models.CharField(primary_key=True, max_length=MAX_LENTH_ID)
     userPassword = models.CharField(max_length=MAX_LENTH_PASSWORD)
+    userGender = (( "男",'male'),( "女",'female'),) #性别
+    userAge = models.IntegerField(blank=True,null=True) #年龄
+    userDepartment = models.CharField(blank=True,null=True,max_length=MAX_LENTH_STRING) #院系
+    userInBlacklist = models.IntegerField() # 默认0 (0 or 1)
